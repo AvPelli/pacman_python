ff7123597dbebf40999cce0f348078b5c8b67d87
Lucas Van de Velde <lucas.vandevelde@ugent.be>
2018-03-08 17:44:48 +0100

    Map nu opgeroepen in game commit

--- Pac-Man/Game.py
+++ Pac-Man/Game.py
@@ -1,21 +1,24 @@
 import pygame  # Importeren van pygame module
 import sys
 import importlib
-from Map import*
+from Map import *
 
-pygame.init()  # Oproepen van de init() functie
+pygame.init()  # Init of pygame
 
-
-
-width = 224*2
-height = 288*2
+width = 224 * 2
+height = 288 * 2
 resolution = (width, height)
 
 gameDisplay = pygame.display.set_mode(resolution)
 pygame.display.set_caption('Pac-Man');
 clock = pygame.time.Clock()
 
+
 class Game():
+
+    def __init__(self):
+        gameDisplay.fill((0, 0, 0))
+
     def run(self):
         running = True
         while running:
@@ -23,17 +26,15 @@ class Game():
                 if event.type == pygame.QUIT:
                     running = False
 
-
-
                 # print(event)
-                pygame.display.update()
+            map = Map(gameDisplay)
+            map.draw_grid()
+            pygame.display.update()
 
-            gameDisplay.fill((0, 0, 0))
-            pygame.draw.rect(gameDisplay, (200, 10, 20), pygame.Rect((100, 300), (20, 30)))
         pygame.quit()
         sys.exit()
 
 
-
 game = Game()
 game.run()
+
--- Pac-Man/Map.py
+++ Pac-Man/Map.py
@@ -5,6 +5,11 @@ from Map import *
 
 pygame.init()
 
+
 class Map():
+    def __init__(self, gameDisplay):
+        self.gameDisplay = gameDisplay  # The display of the game
+
     def draw_grid(self):
-        pygame.draw.line(pygame.display, "black", 250, 400)
+        pygame.draw.line(self.gameDisplay, (200, 10, 20), (0, 8 * 2), (224 * 2, 8 * 2))
+        pygame.display.update()
