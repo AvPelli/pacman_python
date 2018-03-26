6d87928c08462f044ea63607f6a628b55b2000e4
Roel Moeyersoons <Roel.Moeyersoons@UGent.be>
2018-03-26 16:15:27 +0200

    update vloeiend omkeren + commentaar

--- Pac-Man vloeiend/PacMan.py
+++ Pac-Man vloeiend/PacMan.py
@@ -13,18 +13,19 @@ class PacMan:
         self.__coord = coordinate
         self.__direction = Direction.RIGHT
         self.__moving_pos = 0
-        self.__moving = False
+        self.__moving_between_tiles = False
         self.__PIXELSIZE = 16
         self.__speed = 2
+        self.__turnaround = False
 
         # Variables for cosmetics
-        self.__nummer = 1
+        self.__number = 1
         self.dict = self.__image_dict()
         self.__gameDisplay = game_display
         self.imageList = self.dict[self.__direction.get_letter()]
         self.__image = pg.image.load(
             "res/pacman/pacman-{letter} {number}.png".format(letter=self.__direction.get_letter(),
-                                                             number=self.__nummer))
+                                                             number=self.__number))
 
         # Initializes Pacman on give start coordinate
         self.__game = game
@@ -39,18 +40,33 @@ class PacMan:
     # The move method will move Pacman
     def move(self):
         # If pacman is moving between tiles, it will keep going that way until it reaches the next tile
-        # Once there, it's coordinate will be updated so it's ready to be checked in the next iteration (will perform else)
-        if self.__moving:
-            self.__moving_pos += self.__speed
-            self.__image = self.__get_image_direction(self.__direction)
-            if self.__moving_pos >= 16:
-                self.__moving_pos = 0
-                self.__moving = False
-                self.__coord.update_coord(self.__direction)
+        # Once there, it's coordinate will be updated so it's ready to be checked in the next iteration
+        if self.__moving_between_tiles:
+            self.__check_turnaround()
+            if not self.__turnaround:
+                # Proceed to the next tile
+                self.__image = self.__get_image_direction(self.__direction)
+                self.__moving_pos += self.__speed
+                if self.__moving_pos >= 16:
+                    self.__moving_pos = 0
+                    self.__moving_between_tiles = False
+                    self.__coord.update_coord(self.__direction)
+            # However if turnaround has been set (see check_turnaround), pacman will have to move back to the beginning his original tile 1st
+            # Once there he'll set himself ready for the next iteration (and its coordinate will NOT be updated)
+            # To solve issues bug-wise, number will be set on 0 once it has moved back, so its beak is closed at the end, as usual
+            # Also note that, while the image will be reversed, pacman's direction will not be updated until he has finished moving so prevent extra issues that cause all sorts of headaches
+            else:
+                self.__image = self.__get_image_direction(self.__change_direction)
+                self.__moving_pos -= self.__speed
+                if self.__moving_pos <= 0:
+                    self.__moving_pos = 0
+                    self.__moving_between_tiles = False
+                    self.__turnaround = False
+                    self.__number = 0
             self.__set_on_coord(self.__coord)
-        # If pacman is not moving, meaning it's on 1 tile
-        # Checks if it needs to calculate a new coordinate
+        # If pacman is not moving, meaning it's on 1 tile exactly
         # If Pacman is against a wall it will just redraw itself on the same coordinate, but there are not caluclations needed
+        # Else it checks if it needs to calculate a new coordinate, and if a different direction input has been given
         else:
             if not self.__moveable:
                 self.__set_on_coord(self.__coord)
@@ -62,13 +78,13 @@ class PacMan:
 
             # Checks if the new coordinate is a wall
             # If it is a wall, it will not move ( as long as the direction isn't changed)
-            # Else it can start moving there in the next iteration (will perform if self.__moving:)
+            # Else it can start moving there in the next iteration (will perform if self.__moving_between_tiles:)
             if check_next_coord in self.walls:
                 self.__moveable = False
             else:
                 if jump:
                     self.__set_on_opposite_side()
-                self.__moving = True
+                self.__moving_between_tiles = True
             self.__set_on_coord(self.__coord)
             # Moves to the new coordinate
             # Checks if there is candy to eat on the new coordinate
@@ -92,7 +108,7 @@ class PacMan:
     # As soon as it is possible, therefor we check if pacmans its coordinates  added with the direction it would be change to is not a wall
     # If so it will change the moving direction
     def __direction_waiter(self):
-        if not self.__moving:
+        if not self.__moving_between_tiles:
             if self.__change_direction is not None and Coordinate(
                             self.__coord.get_x() + self.__change_direction.value[0],
                             self.__coord.get_y() + self.__change_direction.value[
@@ -119,6 +135,15 @@ class PacMan:
             jump = True
         return Coordinate(newX, newY), jump
 
+    # When pacman is moving between tiles, he should still be able to immediately turn around instead of finishing moving to the next tile 1st,
+    # This method will check if an opposite key has been pressed and sets the variable __turnaround
+    def __check_turnaround(self):
+        if self.__change_direction is not None:
+            x_direction = self.__direction.value[0] + self.__change_direction.value[0]
+            y_direction = self.__direction.value[1] + self.__change_direction.value[1]
+            self.__turnaround = x_direction == 0 and y_direction == 0
+        return None
+
     """"Setters"""
 
     # Setter: this method sets the given direction
@@ -129,7 +154,7 @@ class PacMan:
                       self.__coord.get_y() + direction.value[1]) in self.walls:
             self.__change_direction = direction
             return
-        if self.__moving:
+        if self.__moving_between_tiles:
             self.__change_direction = direction
         else:
             self.__change_direction = None
@@ -148,8 +173,8 @@ class PacMan:
 
     # Getter: Returns the image if needs to use for the give direction (And animation)
     def __get_image_direction(self, direction):
-        self.__nummer = (self.__nummer + 1) % 8
-        return pg.image.load(self.dict[direction.get_letter()][self.__nummer])
+        self.__number = (self.__number + 1) % 8
+        return pg.image.load(self.dict[direction.get_letter()][self.__number])
 
     """"Initialize methods """
 
