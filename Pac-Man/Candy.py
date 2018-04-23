e6d4cfce730c66775a86b9a6b2537ce4eab3e7a6
Lucas Van de Velde <lucas.vandevelde@ugent.be>
2018-04-23 15:20:26 +0200

    SuperCandy visueel op map

--- Pac-Man/Candy.py
+++ Pac-Man/Candy.py
@@ -4,21 +4,21 @@ import pygame as pg
 class Candy:
     # Constructor for candy
     def __init__(self, game_Display, coordinate):
-        self.__game_Display = game_Display
-        self.__coord = coordinate
+        self._game_Display = game_Display
+        self._coord = coordinate
         self.__image = pg.image.load("res/candy/candy {number}.gif".format(number=0))
 
 
     # Draws the candy on a give coordinate
     def draw(self, coordinate):
         (xPixels, yPixels) = (coordinate.get_pixel_tuple())
-        self.__game_Display.blit(self.__image, (xPixels, yPixels))
+        self._game_Display.blit(self.__image, (xPixels, yPixels))
 
     """Getters"""
 
     # Getter, it returns the coordinate of this candy-object
     def get_coord(self):
-        return self.__coord
+        return self._coord
 
     """Overwritten methods"""
 
@@ -27,4 +27,4 @@ class Candy:
     def __eq__(self, other):
         if other == None or not isinstance(other, Candy):
             return False
-        return self.__coord.get_x() == other.get_coord().get_x() and self.__coord.get_y() == other.get_coord().get_y()
+        return self._coord.get_x() == other.get_coord().get_x() and self._coord.get_y() == other.get_coord().get_y()
---
+++ Pac-Man/SuperCandy.py
@@ -0,0 +1,15 @@
+import pygame as pg
+
+from Character import Character
+from Coordinate import Coordinate
+from Direction import Direction
+from Candy import Candy
+
+class SuperCandy(Candy):
+    def __init__(self, game_Display, coordinate):
+        super().__init__(game_Display,coordinate)
+        self._superimage = pg.image.load("res/candy/superdot.png")
+
+    def draw(self, coordinate):
+        (xPixels, yPixels) = (coordinate.get_pixel_tuple())
+        self._game_Display.blit(self._superimage, (xPixels, yPixels))
