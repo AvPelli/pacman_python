8708b804af2a765c60bb4bd88039949e657c4f9d
Arthur Van Pellicom <Arthur.VanPellicom@ugent.be>
2018-05-18 11:01:44 +0200

    Documentatie character,direction en coordinate

--- Pac-Man/Character.py
+++ Pac-Man/Character.py
@@ -7,6 +7,10 @@ from Direction import Direction
 
 class Character(ABC):
     def __init__(self, PIXELSIZE, speed, moving_pos, direction, game, coordinate):
+        """
+        Creates a Character object, Ghost and Pacman subclasses also use the variables here, so they are protected.
+        :return: void
+        """
         # protected variables for all the subclasses
         self._speed = speed
         self._PIXELSIZE = PIXELSIZE
@@ -18,9 +22,14 @@ class Character(ABC):
         self._coord = coordinate
         self.start_coord = deepcopy(coordinate)
 
-    # It wil draw a Character(image) of the given coordinate on the given game_display
-    # Protected method: It can and only will be used in subclasses. Prohibited to be used outside these subclasses
     def _draw_character(self, coordinate, image):
+        """
+        Draw a Character(image) of the given coordinate on the given game_display
+        Protected method: It can and only will be used in subclasses. Prohibited to be used outside these subclasses
+        :param: Coordinate: where to draw the object
+        :param: image: what image to draw
+        :return: void
+        """
         (xPixels, yPixels) = (coordinate.get_pixel_tuple())
         # self.check_reset()
         if self._direction is not None:
@@ -32,9 +41,12 @@ class Character(ABC):
     def move(self):
         pass
 
-    # Method used for character while they move between tiles (Base model of the method)
-    # Each subclass expands this method
     def _move_between_tiles(self):
+        """
+        Method used for character while they move between tiles (Base model of the method)
+        each subclass expands this method
+        :return: void
+        """
         self._moving_pos += self._speed
         if self._moving_pos >= 16:
             self._moving_pos = 0
@@ -42,8 +54,11 @@ class Character(ABC):
             # Once there, it's coordinate will be updated so it's ready to be checked in the else: part of move
             self._coord.update_coord(self._direction)
 
-    # Set the character on the other side
     def _set_on_opposite_side(self):
+        """
+        Set the character on the other side
+        :return: void
+        """
         (maxX, maxY) = self._game.get_max()
         (x, y) = (self._coord.get_coord_tuple())
 
@@ -54,9 +69,12 @@ class Character(ABC):
             self._direction = Direction.RIGHT
             self._coord = Coordinate(0, y)
 
-    # Calculates the next coordinate
-    # Also this method checks if it is a "teleporter" which will perform __set_on_opposite_side() in move() method
     def _calculate_new_coord(self):
+        """
+        Calculates the next coordinate also this method checks if it is a "teleporter"
+        which will perform __set_on_opposite_side() in move() method
+        :return: Coordinate
+        """
         (maxX, maxY) = self._game.get_max()
         (x, y) = (self._coord.get_coord_tuple())
         addX, addY = self._direction.value
@@ -67,16 +85,27 @@ class Character(ABC):
             newX, newY = 0, 0
         return (newX, newY), jump
 
-    # Base model of a method that reset the character to the begin status
     def reset_character(self):
+        """
+        Base model of a method that reset the character to the begin status
+        :return: void
+        """
         # deepcopy, or else the attribute __coord will be a reference to the attribute start_coord
         # this way when pacman gets caught the second time, it will "reset" to the coordinate it already stands on
         self._coord = deepcopy(self.start_coord)
 
     def get_coord(self):
+        """
+        Getter
+        :return: Coordinate
+        """
         return deepcopy(self._coord)
 
     def get_direction(self):
+        """
+        Getter
+        :return: Direction
+        """
         return deepcopy(self._direction)
 
     def set_speed(self, speedvalue):
--- Pac-Man/Coordinate.py
+++ Pac-Man/Coordinate.py
@@ -1,6 +1,13 @@
 class Coordinate:
     # Constructor of Coordinate
     def __init__(self, x, y, is_wall=False):
+        """
+        Creates Coordinate object, each coordinate also holds a boolean that tells if the coordinate is a wall or not.
+        :param: x (Coordinate)
+        :param: y (Coordinate)
+        :param: is_wall : boolean
+        :return: void
+        """
         self.__x = x
         self.__y = y
         self.__PIXELSIZE = 16
@@ -12,42 +19,64 @@ class Coordinate:
 
     """Getters"""
 
-    # Getter: returns if there is a wall on this coordinate
     def is_wall(self):
+        """
+        Checks if Coordinate is a wall
+        :return: boolean
+        """
         return self.__is_wall
 
-    # Getter: returns x value
     def get_x(self):
+        """
+        Getter
+        :return: x Coordinate
+        """
         return self.__x
 
-    # Getter: returns y value
     def get_y(self):
+        """
+        Getter
+        :return: y Coordinate
+        """
         return self.__y
 
-    # Getter: returns a tuple (Pixel X, Pixel Y)
     def get_pixel_tuple(self):
+        """
+        Getter
+        :return: tuple (Pixel x, Pixel y)
+        """
         # Formule voor van coord systeem --> naar pixels
         return int((self.__x * self.__PIXELSIZE)), int(
             (self.__y * self.__PIXELSIZE) + 3 * self.__PIXELSIZE)
 
-    # Getter: returns a tuple (X, Y)
     def get_coord_tuple(self):
+        """
+        Getter
+        :return: tuple (Coordinate X, Coordinate Y)
+        """
         return self.__x, self.__y
 
     """Overwritten methods"""
 
-    # Override the equal methode
-    # 2 Coordinate objects are the same if x and y of both objects are the same
     def __eq__(self, other):
+        """
+        Override the equal methode 2 Coordinate objects are the same if x and y of both objects are the same
+        :return: string
+        """
         if not isinstance(other, Coordinate):
             return False
         return self.__x == other.get_x() and self.__y == other.get_y()
 
-    # Override the hashcode
-    # This is needed for dicitionaries like the candy dictionary
     def __hash__(self) -> int:
+        """
+        Override the hashcode this is needed for dicitionaries like the candy dictionary
+        :return: int
+        """
         return (int)(self.__x * 31 + self.__y * 7 + 1)
 
-    # to string method of Coordinate
     def __str__(self) -> str:
+        """
+        to string method of Coordinate
+        :return: string
+        """
         return "Coordinate: X: {x},   Y: {y}".format(x=self.__x, y=self.__y)
