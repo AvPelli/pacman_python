b758d1d8f6d322810e8cf0f5ab34862602a0a0ee
Aleandro Delys <Aleandro.Delys@ugent.be>
2018-05-19 17:51:50 +0200

    Aanpassing commentaar Gate en SuperCandy

--- Pac-Man/Gate.py
+++ Pac-Man/Gate.py
@@ -1,28 +1,46 @@
 class Gate():
 
-    # Constructor of Gate (2 Coordinates are given)
     def __init__(self, port1, port2):
+        """
+        Constructor of Gate (2 Coordinates are given)
+        :param port1: type: Coordinate
+        :param port2: type: Coordinate
+        """
         self.__port1 = port1
         self.__port2 = port2
 
-    # Gives the other port, if port1 is given with this method it will return port2 and vice versa
     def give_other_port(self, port):
+        """
+        Gives the other port, if port1 is given as parameter, this method will return port2 and vice versa
+        :param port: type: Coordinate
+        :return: Coordinate
+        """
         return self.__port2 if port == self.__port1 else self.__port1
 
     """"Getters"""
 
-    # Getter: returns a tuple of coordinates which are part of this Gate
     def get_coordinates(self):
+        """
+        Returns a tuple of coordinates which are part of this Gate
+        :return: tuple
+        """
         return self.__port1, self.__port2
 
     """Overwritten methods"""
 
-    # Override the equals method
-    # 2 Gates object are the same if they the same 2 Coordinates
     def __eq__(self, other) -> bool:
+        """
+        Override the equals method
+        2 Gates object are the same if they the same 2 Coordinates
+        :param other: type: Gate
+        :return: boolean
+        """
         return (self.__port1 == other.__port1 and self.__port2 == other.__port2) or (
                 self.__port1 == other.__port2 and self.__port2 == other.__port1)
 
-    # Override the hashcode
     def __hash__(self) -> int:
+        """
+        Override the hashcode
+        :return: int
+        """
         return self.__port1 * self.__port2
--- Pac-Man/SuperCandy.py
+++ Pac-Man/SuperCandy.py
@@ -4,9 +4,20 @@ from Candy import Candy
 
 
 class SuperCandy(Candy):
-    def __init__(self, game_Display, coordinate):
-        super().__init__(game_Display, coordinate)
+    def __init__(self, game_display, coordinate):
+        """
+        Constructor of SuperCandy, a subclass of Candy
+        When eaten it allows the player to start frightened mode and he can start eating the ghosts
+        :param game_display:
+        :param coordinate:
+        """
+        super().__init__(game_display, coordinate)
         self._image = pg.image.load("res/candy/superdot.png")
+        self.__score = 100
 
     def get_score(self):
-        return 100
+        """
+        Returns the score the player gets, when he eats a SuperCandy object
+        :return: int
+        """
+        return self.__score
